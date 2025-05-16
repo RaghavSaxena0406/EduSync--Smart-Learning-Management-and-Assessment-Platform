1504fab0765f38ca28de91f14c661478386b04ac
RaghavSaxena0406 <raghavsaxena.ai25@jecrc.ac.in>
2025-05-16 10:14:50 +0530

    added database backup

---
+++ Backend/EduSyncWebApi/DTO/AssessmentDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace EduSyncWebApi.DTO
+{
+    public class AssessmentDTO
+    {
+        public Guid AssessmentId { get; set; }
+
+        public Guid? CourseId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Questions { get; set; }
+
+        public int? MaxScore { get; set; }
+    }
+}
---
+++ Backend/EduSyncWebApi/DTO/ResultDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace EduSyncWebApi.DTO
+{
+    public class ResultDTO
+    {
+        public Guid ResultId { get; set; }
+
+        public Guid? AssessmentId { get; set; }
+
+        public Guid? UserId { get; set; }
+
+        public int? Score { get; set; }
+
+        public DateTime? AttemptDate { get; set; }
+    }
+}
