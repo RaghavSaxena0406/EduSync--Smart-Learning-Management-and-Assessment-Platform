d4b5498d5fdf7072cf055ff86965994efcfa015a
RaghavSaxena0406 <raghavsaxena.ai25@jecrc.ac.in>
2025-05-21 23:59:24 +0530

    frontend added

---
+++ Frontend/edusync-project/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Courses from './components/courses/Courses';
+import CourseDetail from './components/courses/CourseDetail';
+import Assessments from './components/assessments/Assessments';
+import CreateAssessment from './components/assessments/CreateAssessment';
+import EditAssessment from './components/assessments/EditAssessment';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        {/* Public routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        {/* Protected routes */}
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Courses />
+          </ProtectedRoute>
+        } />
+        <Route path="/courses" element={
+          <ProtectedRoute>
+            <Courses />
+          </ProtectedRoute>
+        } />
+        <Route path="/courses/:id" element={
+          <ProtectedRoute>
+            <CourseDetail />
+          </ProtectedRoute>
+        } />
+        <Route path="/assessments/course/:courseId" element={
+          <ProtectedRoute>
+            <Assessments />
+          </ProtectedRoute>
+        } />
+        <Route path="/assessments/course/:courseId/create" element={
+          <ProtectedRoute>
+            <CreateAssessment />
+          </ProtectedRoute>
+        } />
+        <Route path="/assessments/course/:courseId/edit/:assessmentId" element={
+          <ProtectedRoute>
+            <EditAssessment />
+          </ProtectedRoute>
+        } />
+        
+        {/* Catch all route - redirect to login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
